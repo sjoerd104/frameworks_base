906f8108a4bdad9af12c4323a52dce90bb0aea9b
Tim Zimmermann <tim@linux4.de>
2022-06-01 16:50:21 +0200

    Settings: Add System.HIGH_TOUCH_SENSITIVITY_ENABLE
    Change-Id: I8450b2eda890e99734a4dd85b1e88c3ed0a39815

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5557,6 +5557,14 @@ public final class Settings {
         public static final String TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
 
         /**
+         * Whether the HighTouchSensitivity is activated or not.
+         * 0 = off, 1 = on
+         * @hide
+         */
+        public static final String HIGH_TOUCH_SENSITIVITY_ENABLE =
+                "high_touch_sensitivity_enable";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
